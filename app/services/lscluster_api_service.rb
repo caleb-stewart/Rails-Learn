abc548999056c4c4283e34e0226de565cb1d6ab9
Caleb Stewart <calebstew32@hotmail.com>
2025-05-11 12:13:34 -0700

    HTTParty working better

--- app/services/lscluster_api_service.rb
+++ app/services/lscluster_api_service.rb
@@ -1,10 +1,14 @@
 class LsclusterApiService
+  # How we make HTTP requests to external APIs
   include HTTParty
-  base_uri 'https://lscluster.hockeytech.com/feed/'
+  # class method provided by HTTParty to set the base URI for all requests in this class
+  base_uri('https://lscluster.hockeytech.com/feed/')
   
   def initialize
+    # Secret key, that is actually public from the WHL site
     @key = ENV['LSCLUSTER_API_KEY']
-    puts @key
+
+    # Parameters always used in the URL
     @default_params = {
       client_code: 'whl',
       fmt: 'json', 
@@ -17,29 +21,33 @@ class LsclusterApiService
 
   def game_id_url(num_of_days_ahead, num_of_past_games, current_team_id = '')
 
-    response = HTTParty.get("https://lscluster.hockeytech.com/feed/?feed=modulekit&key=#{@key}&view=scorebar&client_code=whl&numberofdaysahead=#{num_of_days_ahead}&numberofdaysback=#{num_of_past_games}&season_id=&team_id=#{current_team_id}&lang_code=en&fmt=json")
-    
-    puts response.body
+    # This is all of the parameters that are used in the URL
+    query = @default_params.merge( {
+      feed: 'modulekit',
+      view: 'scorebar',
+      numberofdaysahead: num_of_days_ahead,
+      numberofdaysback: num_of_past_games,
+      season_id: '',
+      team_id: current_team_id
+    })
 
-    # query = @default_params.merge( {
-    #   feed: 'modulekit',
-    #   view: 'scorebar',
-    #   numberofdaysahead: num_of_days_ahead,
-    #   numberofdaysback: num_of_past_games,
-    #   season_id: '',
-    #   team_id: current_team_id
-    # })
-
-    # self.class.get('', query: query)
+    # HTTParty appends '' to base_uri
+    # and then appends the query string to that
+    # so we need to pass an empty string to the get method
+    self.class.get('', query: query)
   end
 
   def get_game_stats_url(game_id)
+    # Append the parameters to the base URI
     query = @default_params.merge( {
       feed: 'gc', 
       game_id: game_id,
       tab: 'clock'
     })
 
+    # HTTParty appends '' to base_uri
+    # and then appends the query string to that
+    # so we need to pass an empty string to the get method
     self.class.get('', query: query)
   end
 end
